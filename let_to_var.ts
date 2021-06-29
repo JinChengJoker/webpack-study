d1a245befd0c0569b9040cbf6c26686b5a123d71
cheng <jinchengjoker@foxmail.com>
2021-06-29 16:32:34 +0800

    first commit

---
+++ let_to_var.ts
@@ -0,0 +1,21 @@
+import { parse } from '@babel/parser'
+import traverse from '@babel/traverse'
+import generator from '@babel/generator'
+
+const code = `let a = 'let'; let b = 2`
+
+const ast = parse(code, { sourceType: 'module' })
+
+traverse(ast, {
+  enter: item => {
+    if (item.node.type === 'VariableDeclaration') {
+      if (item.node.kind === 'let') {
+        item.node.kind = 'var'
+      }
+    }
+  }
+})
+
+const result = generator(ast, {}, code)
+
+console.log(result.code)
