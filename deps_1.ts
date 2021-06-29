b9adc7920b8c4e3c398b57fe34f47694fc302be8
cheng <jinchengjoker@foxmail.com>
2021-06-29 18:08:08 +0800

    分析文件依赖

---
+++ deps_1.ts
@@ -0,0 +1,35 @@
+import { parse } from "@babel/parser";
+import traverse from "@babel/traverse";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+type DepRelation = {
+  deps: string[],
+  code: string
+}
+
+const projectRoot = resolve(__dirname, './deps_1')
+
+const depRelation: DepRelation = {
+  deps: [],
+  code: '',
+}
+
+const collectDepsAndCode = (filepath: string) => {
+  const code = readFileSync(filepath).toString()
+  const ast = parse(code, { sourceType: 'module' })
+
+  depRelation.code = code
+
+  traverse(ast, {
+    enter: item => {
+      if (item.node.type === 'ImportDeclaration') {
+        depRelation.deps.push(item.node.source.value)
+      }
+    }
+  })
+}
+
+collectDepsAndCode(resolve(projectRoot, 'index.js'))
+
+console.log(depRelation)
